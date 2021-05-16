9421bb7fb75fc4a48af66dc212642c3aed7bbbf7
Daniel Willis <kxt2757@autuni.ac.nz>
2021-05-16 16:51:56 +1200

    Added Images and detail to the starting page GUI

--- Game/src/GUI/GameSetup.java
+++ Game/src/GUI/GameSetup.java
@@ -1,161 +1,165 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package GUI;
 
-
 import java.awt.*;
-import java.awt.Graphics;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.*;
-import javax.swing.border.Border;
-
 
 /**
  *
  * @author liamo
  */
-public class GameSetup implements ActionListener {
-    
-    private JFrame frame1;
-    private JPanel panel1;
-    private JPanel panel2;
-    private JPanel panel1_5;
-    
-    private Image img;
-    
-    private JFrame frame2;
-    private JPanel panel4;
-    private JPanel panel5;
-    private JPanel panel6;
+public class GameSetup {
 
-    private JLabel label3;
-    private JLabel label4;
-    
-    
-    public GameSetup() throws IOException{
-        frame1 = new JFrame();
-        
-        
+    private final int startPageWidth;
+    private final int startPageHeight;
+
+    private JFrame gameplayFrame;
+
+    public GameSetup() throws IOException {
+        startPageWidth = 700;
+        startPageHeight = 670;
+
         JButton b1 = new JButton("Warrior");
         JButton b2 = new JButton("Elf");
         JButton b3 = new JButton("Dwarf");
-        
-        b1.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e) {
-                frame2.setVisible(true);
-            }
+
+        b1.addActionListener((ActionEvent e) -> {
+            gameplayFrame.setVisible(true);
         });
-        
-        b2.addActionListener(this);
-        b3.addActionListener(this);
-        
-        JLabel label1 = new JLabel("Welcome to RPGame GUI.");
-        
-        //myPicture = ImageIO.read(new File("src/mountains.png"));
-        //JLabel picLabel = new JLabel(new ImageIcon(myPicture));
-        //picLabel.setBorder(BorderFactory.createEmptyBorder(0, 0, 300, 600));
-        
-        
-        panel1 = new JPanel();
-        panel1.setBackground(Color.GRAY);
-        panel1.setPreferredSize(new Dimension(600, 300));
-        panel1.setBorder(BorderFactory.createEmptyBorder(100, 200, 100, 100));
-        panel1.setLayout(new BorderLayout());
-        panel1.add(label1);
-        //panel1.add(picLabel);
-        
 
-        panel1_5 = new JPanel();
-        panel1_5.setBackground(Color.black);
-        panel1_5.setPreferredSize(new Dimension(600, 40));
-        
-        panel2 = new JPanel();
-        JLabel label2 = new JLabel("Choose your Character: ");
-        
-        panel2.setBackground(Color.LIGHT_GRAY);
-        panel2.setPreferredSize(new Dimension(600, 300));
-        panel2.setBorder(BorderFactory.createEmptyBorder(100,100,100,100));
-        panel2.add(label2);
-        panel2.add(b1);
-        panel2.add(b2);
-        panel2.add(b3);
-        
-        frame1 = new JFrame();
-        frame1.add(panel1, BorderLayout.NORTH);
-        frame1.add(panel1_5, BorderLayout.CENTER);
-        frame1.add(panel2, BorderLayout.SOUTH);
-        frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame1.setTitle("Role Player Game GUI");
-        frame1.pack();
-        frame1.setVisible(true);    
-        
-        
-        
-        
-    
-        frame2 = new JFrame();
-        
-        JButton left = new JButton("a");
-        JButton right = new JButton("f");
-        JButton attack = new JButton("w");
-        JButton heal = new JButton("s");
-        
-        left.addActionListener(this);
-        
-        panel4 = new JPanel();
-        panel4.setPreferredSize(new Dimension(800, 80));
-        panel4.setBackground(Color.darkGray);
-        
-        label3 = new JLabel("Find your way out of the dungeon. Defeat all the enemies: ");
-        label3.setForeground(Color.white);
-        panel4.add(label3, BorderLayout.CENTER);
-        
-        panel5 = new JPanel();
-        panel5.setBackground(Color.white);
-        panel5.setPreferredSize(new Dimension(800, 800));
-        
-        panel6 = new JPanel();
-        panel6.setPreferredSize(new Dimension(800, 120));
-        panel6.setBackground(Color.LIGHT_GRAY);
-        panel6.setLayout(new FlowLayout());
-        label4 = new JLabel("Pick an action");
-        label4.setForeground(Color.red);
-        panel6.add(label4, BorderLayout.LINE_START);
-        panel6.add(left, BorderLayout.WEST);
-        panel6.add(right, BorderLayout.EAST);
-        panel6.add(attack, BorderLayout.NORTH);
-        panel6.add(heal, BorderLayout.SOUTH);
-        
-        frame2 = new JFrame();
-        frame2.add(panel4, BorderLayout.NORTH);
-        frame2.add(panel5, BorderLayout.CENTER);
-        frame2.add(panel6, BorderLayout.SOUTH);
-        frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame2.setTitle("Role Player Game GUI");
-        frame2.pack();
-        frame2.setVisible(false);
-        
-        
-    }
+        b2.addActionListener((ActionEvent e) -> {
+            gameplayFrame.setVisible(true);
+        });
 
-    public void addImg() {
-        try {
-            img = ImageIO.read(new File("src/mountains.png"));
-        } catch (IOException ex){}
-    }
-    
-    public void paintComponent(Graphics g) {
-        this.paintComponent(g);
-        g.drawImage(img, 0, 0, this.panel1);
+        b3.addActionListener((ActionEvent e) -> {
+            gameplayFrame.setVisible(true);
+        });
+
+        JLabel welcomeLabel = new JLabel("Welcome to the RPG Game GUI");
+        welcomeLabel.setHorizontalAlignment(JLabel.CENTER);
+        welcomeLabel.setForeground(Color.white);
+        welcomeLabel.setFont(welcomeLabel.getFont().deriveFont(22f));
+
+        BufferedImage mountains = ImageIO.read(new File("./mountains.png"));
+        JLabel mountainsImage = new JLabel(new ImageIcon(mountains));
+        mountainsImage.setPreferredSize(new Dimension(startPageWidth, 300));
+        mountainsImage.setLayout(new BorderLayout());
+
+        mountainsImage.add(welcomeLabel);
+
+        JPanel startPageTopPanel = new JPanel();
+        startPageTopPanel.setBackground(Color.GRAY);
+        startPageTopPanel.setPreferredSize(new Dimension(startPageWidth, 300));
+        startPageTopPanel.add(mountainsImage);
+
+        JLabel characterLabel = new JLabel("Choose your Character: ");
+        characterLabel.setForeground(Color.white);
+
+        JPanel startPageCenterPanel = new JPanel();
+        startPageCenterPanel.setBackground(Color.black);
+        startPageCenterPanel.add(characterLabel);
+        startPageCenterPanel.add(b1);
+        startPageCenterPanel.add(b2);
+        startPageCenterPanel.add(b3);
+
+        JPanel startPageBottomPanel = new JPanel();
+        startPageBottomPanel.setBackground(Color.LIGHT_GRAY);
+        startPageBottomPanel.setPreferredSize(new Dimension(startPageWidth, 300));
+        
+        JPanel WarriorPanel = new JPanel();
+        WarriorPanel.setBackground(Color.LIGHT_GRAY);
+        WarriorPanel.setPreferredSize(new Dimension(225, 300));
+        JLabel WarriorStats = new JLabel("<html><b>Warrior:</b><br/>Health: 100<br/>Attack: 20</html>s");
+        WarriorStats.setFont(WarriorStats.getFont().deriveFont(20f));
+        BufferedImage warrior = ImageIO.read(new File("./warrior.png"));
+        JLabel WarriorImage = new JLabel(new ImageIcon(warrior));
+        WarriorPanel.add(WarriorStats, BorderLayout.NORTH);
+        WarriorPanel.add(WarriorImage, BorderLayout.SOUTH);
+        
+        JPanel ElfPanel = new JPanel();
+        ElfPanel.setBackground(Color.LIGHT_GRAY);
+        ElfPanel.setPreferredSize(new Dimension(225, 300));
+        JLabel ElfStats = new JLabel("<html><b>Elf:</b><br/>Health: 75<br/>Attack: 30</html>s");
+        ElfStats.setFont(ElfStats.getFont().deriveFont(20f));
+        BufferedImage elf = ImageIO.read(new File("./elf.png"));
+        JLabel ElfImage = new JLabel(new ImageIcon(elf));
+        ElfPanel.add(ElfStats, BorderLayout.NORTH);
+        ElfPanel.add(ElfImage, BorderLayout.SOUTH);
+        
+        JPanel DwarfPanel = new JPanel();
+        DwarfPanel.setBackground(Color.LIGHT_GRAY);
+        DwarfPanel.setPreferredSize(new Dimension(225, 300));
+        JLabel DwarfStats = new JLabel("<html><b>Dwarf:</b><br/>Health: 125<br/>Attack: 10</html>s");
+        DwarfStats.setFont(DwarfStats.getFont().deriveFont(20f));
+        BufferedImage dwarf = ImageIO.read(new File("./dwarf.png"));
+        JLabel DwarfImage = new JLabel(new ImageIcon(dwarf));
+        DwarfPanel.add(DwarfStats, BorderLayout.NORTH);
+        DwarfPanel.add(DwarfImage, BorderLayout.SOUTH);
+        
+        startPageBottomPanel.add(WarriorPanel, BorderLayout.WEST);
+        startPageBottomPanel.add(ElfPanel, BorderLayout.CENTER);
+        startPageBottomPanel.add(DwarfPanel, BorderLayout.EAST);
+
+        JFrame startPageFrame = new JFrame();
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        startPageFrame.setLocation(dim.width / 2 - startPageWidth / 2, dim.height / 2 - startPageHeight / 2);
+        startPageFrame.setPreferredSize(new Dimension(startPageWidth, startPageHeight));
+        startPageFrame.setResizable(false);
+        startPageFrame.add(startPageTopPanel, BorderLayout.NORTH);
+        startPageFrame.add(startPageCenterPanel, BorderLayout.CENTER);
+        startPageFrame.add(startPageBottomPanel, BorderLayout.SOUTH);
+        startPageFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        startPageFrame.setTitle("Role Player Game GUI");
+        startPageFrame.pack();
+        startPageFrame.setVisible(true);
+
+        gameplayFrame = new JFrame();
+
+        JButton left = new JButton("Move Left");
+        JButton right = new JButton("Move Right");
+        JButton attack = new JButton("Attack");
+        JButton heal = new JButton("Heal");
+
+        left.addActionListener((ActionEvent e) -> {
+            //Add action
+        });
+
+        JPanel gameplayTopPanel = new JPanel();
+        gameplayTopPanel.setPreferredSize(new Dimension(800, 80));
+        gameplayTopPanel.setBackground(Color.darkGray);
+
+        JLabel objectiveLabel = new JLabel("Find your way out of the dungeon. Defeat all the enemies: ");
+        objectiveLabel.setForeground(Color.white);
+        gameplayTopPanel.add(objectiveLabel, BorderLayout.CENTER);
+
+        JPanel gameplayCenterPanel = new JPanel();
+        gameplayCenterPanel.setBackground(Color.white);
+        gameplayCenterPanel.setPreferredSize(new Dimension(800, 800));
+
+        JPanel gameplayBottomPanel = new JPanel();
+        gameplayBottomPanel.setPreferredSize(new Dimension(800, 120));
+        gameplayBottomPanel.setBackground(Color.LIGHT_GRAY);
+        gameplayBottomPanel.setLayout(new FlowLayout());
+        JLabel actionLabel = new JLabel("Pick an action");
+        actionLabel.setForeground(Color.red);
+        gameplayBottomPanel.add(actionLabel, BorderLayout.LINE_START);
+        gameplayBottomPanel.add(left, BorderLayout.WEST);
+        gameplayBottomPanel.add(right, BorderLayout.EAST);
+        gameplayBottomPanel.add(attack, BorderLayout.NORTH);
+        gameplayBottomPanel.add(heal, BorderLayout.SOUTH);
+
+        gameplayFrame = new JFrame();
+        gameplayFrame.add(gameplayTopPanel, BorderLayout.NORTH);
+        gameplayFrame.add(gameplayCenterPanel, BorderLayout.CENTER);
+        gameplayFrame.add(gameplayBottomPanel, BorderLayout.SOUTH);
+        gameplayFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gameplayFrame.setTitle("Role Player Game GUI");
+        gameplayFrame.pack();
+        gameplayFrame.setVisible(false);
 
     }
 }
--- Game/src/GUI/Main.java
+++ Game/src/GUI/Main.java
@@ -1,9 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package GUI;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -15,8 +14,10 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        GameSetup setup = new GameSetup();
-        
+        try {
+            GameSetup setup = new GameSetup();
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
 }
