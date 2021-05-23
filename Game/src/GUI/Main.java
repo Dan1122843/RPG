de858107d3c91c5575deb1e138634c8ce87f2499
Daniel Willis <83578170+Dan1122843@users.noreply.github.com>
2021-05-24 10:51:32 +1200

    Revert "Liam"

--- Game/src/GUI/Main.java
+++ Game/src/GUI/Main.java
@@ -1,10 +1,8 @@
 package GUI;
 
 import java.io.IOException;
-
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 
 /**
  *
@@ -15,17 +13,11 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-
-    public static void main(String[] args) throws IOException {
-        GameSetup setup = new GameSetup();
-        
-
     public static void main(String[] args) {
         try {
             GameSetup setup = new GameSetup();
         } catch (IOException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
-
     }
 }
